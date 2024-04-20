ac9ddc733664c64a43036f359ced2738d42b7d50
ttshenmon <167599957+ttshenmon@users.noreply.github.com>
2024-04-20 09:42:42 -0400

    Update Mod.cs

--- Mod.cs
+++ Mod.cs
@@ -1,93 +1,85 @@
-using System;
-using System.IO;
-using BattleThemes.Template.Template;
+﻿using BattleThemes.Template.Template;
 using BattleThemes.Template.Template.Configuration;
 using BGME.BattleThemes.Config;
 using Reloaded.Hooks.ReloadedII.Interfaces;
 using Reloaded.Mod.Interfaces;
 using System.ComponentModel;
 
-namespace BattleThemes.Template
+namespace BattleThemes.Template;
+
+public class Mod : ModBase
 {
-    public class Mod : ModBase
+    private readonly IModLoader modLoader;
+    private readonly IReloadedHooks? hooks;
+    private readonly ILogger log;
+    private readonly IMod owner;
+
+    private Config config;
+    private readonly IModConfig modConfig;
+
+    private readonly ThemeConfig themeConfig;
+
+    public Mod(ModContext context)
     {
-        private readonly IModLoader? modLoader;
-        private readonly IReloadedHooks? hooks;
-        private readonly ILogger? log;
-        private readonly IMod? owner;
+        this.modLoader = context.ModLoader;
+        this.hooks = context.Hooks;
+        this.log = context.Logger;
+        this.owner = context.Owner;
+        this.config = context.Configuration;
+        this.modConfig = context.ModConfig;
 
-        private Config? config;
-        private readonly IModConfig? modConfig;
+        this.themeConfig = new(this.modLoader, this.modConfig, this.config, this.log);
 
-        private readonly ThemeConfig? themeConfig;
+        /* Connect the battle theme files to the config. */
+        /* Steps:
+         * 1. Place battle theme files in: MOD_FOLDER/battle-themes/options
+         * 2. Add a config setting for it in: public class Config : Configurable<Config>
+         * 3. Edit/copy and paste the line below with your new setting and the theme file it enables.
+         * 
+         * For example, right now the config has a "P4G" setting which enables "p4g.theme.pme" in the options folder.
+         */
 
-        public Mod(ModContext context)
-        {
-            this.modLoader = context.ModLoader;
-            this.hooks = context.Hooks;
-            this.log = context.Logger;
-            this.owner = context.Owner;
-            this.config = context.Configuration;
-            this.modConfig = context.ModConfig;
+        this.themeConfig.AddSetting(nameof(this.config.kpop), "kpop.theme.pme");
+        this.themeConfig.AddSetting(nameof(this.config.kpop), "aespa.theme.pme");
+        this.themeConfig.AddSetting(nameof(this.config.kpop), "beast.theme.pme");
+        this.themeConfig.AddSetting(nameof(this.config.kpop), "bts.theme.pme");
+        this.themeConfig.AddSetting(nameof(this.config.kpop), "gidle.theme.pme");
+        this.themeConfig.AddSetting(nameof(this.config.kpop), "got7.theme.pme");
+        this.themeConfig.AddSetting(nameof(this.config.kpop), "lesserafim.theme.pme");
+        this.themeConfig.AddSetting(nameof(this.config.kpop), "nmixx.theme.pme");
+        this.themeConfig.AddSetting(nameof(this.config.kpop), "redvelvet.theme.pme");
+        this.themeConfig.AddSetting(nameof(this.config.kpop), "shinee.theme.pme");
+        this.themeConfig.AddSetting(nameof(this.config.kpop), "stayc.theme.pme");
 
-            this.themeConfig = new(this.modLoader!, this.modConfig!, this.config!, this.log!);
 
-            string modDirectory = @"E:\Reloaded2\Mods";
-            string optionsDirectory = Path.Combine(modDirectory, "sees.costume.kpop", "battle-themes", "music");
-
-            string[] themeFiles = Directory.GetFiles(optionsDirectory);
-
-            foreach (string file in themeFiles)
-            {
-                string fileName = Path.GetFileName(file);
-                this.themeConfig.AddSetting(nameof(Config.Aespa), fileName); // Replace Config.Aespa with appropriate names for each song
-            }
-
-            this.themeConfig.Initialize();
-        }
-
-        public override void ConfigurationUpdated(Config configuration)
-        {
-            config = configuration;
-            log?.WriteLine($"[{modConfig?.ModId}] Config Updated: Applying");
-        }
-
-        public Mod() { }
+        /*-------------------------------------------------------*/
+        this.themeConfig.Initialize();
     }
 
-    public class Config : Configurable<Config>
+    #region Standard Overrides
+    public override void ConfigurationUpdated(Config configuration)
     {
-        /* ADD CONFIG SETTINGS HERE */
-
-        // Replace the configuration settings with appropriate names for your songs
-        [DefaultValue(true)]
-        public bool Aespa { get; set; } = true;
-
-        [DefaultValue(true)]
-        public bool Beast { get; set; } = true;
-
-        [DefaultValue(true)]
-        public bool Bts { get; set; } = true;
-
-        [DefaultValue(true)]
-        public bool Gidle { get; set; } = true;
-
-        [DefaultValue(true)]
-        public bool Got7 { get; set; } = true;
-
-        [DefaultValue(true)]
-        public bool Lesserafim { get; set; } = true;
-
-        [DefaultValue(true)]
-        public bool Nmixx { get; set; } = true;
-
-        [DefaultValue(true)]
-        public bool Redvelvet { get; set; } = true;
-
-        [DefaultValue(true)]
-        public bool Shinee { get; set; } = true;
-
-        [DefaultValue(true)]
-        public bool Stayc { get; set; } = true;
+        // Apply settings from configuration.
+        // ... your code here.
+        config = configuration;
+        log.WriteLine($"[{modConfig.ModId}] Config Updated: Applying");
     }
+    #endregion
+
+    #region For Exports, Serialization etc.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public Mod() { }
+#pragma warning restore CS8618
+    #endregion
+}
+
+public class Config : Configurable<Config>
+{
+    /* ADD CONFIG SETTINGS HERE */
+
+    [Category("Aespa")]
+    [DisplayName("Black Mamba")]
+    [Description("plays black mamba by aespa")]
+    [DefaultValue(true)]
+    public bool aespa { get; set; } = true;
 }
